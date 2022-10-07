0b212d0559a28b714ed31750cb0d226ba8ef46ca
Fatih Emin KARAHAN <fatih81443@gmail.com>
2022-10-07 10:17:25 +0300

    Daire

---
+++ Java/Daire/DaireAlani.java
@@ -0,0 +1,14 @@
+package Java.Daire;
+import java.util.Scanner;
+public class DaireAlani {
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Dairenin yarıçapını giriniz : ");
+		int r = scanner.nextInt();
+		System.out.print("Dairenin merkez açı ölçüsünü giriniz : ");
+		int a = scanner.nextInt();
+		double pi = 3.14;
+		double daireDilimininAlani = (pi * (r * r) * a) / 360;
+		System.out.println("Daire diliminin alanı : " + daireDilimininAlani);
+	}    
+}
