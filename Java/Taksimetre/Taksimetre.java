2a9f549e13d4fc60fe8827d888a68481caf0a5c2
Fatih Emin KARAHAN <fatih81443@gmail.com>
2022-10-07 10:12:31 +0300

    Taksimetre

---
+++ Java/Taksimetre/Taksimetre.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        double perKm=2.20,km,total,startPrice=10,price;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Gidilen Mesafeyi Yazınız:");
+        km = input.nextDouble();
+        total = startPrice + km*perKm;
+        price = total > 20 ? total : 20;
+        System.out.println("Ödenecek Tutar:"+ price);
+    }
+}
